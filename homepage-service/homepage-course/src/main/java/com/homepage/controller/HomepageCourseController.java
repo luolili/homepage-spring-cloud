90f495332af489ece0b25b7a101353318b245068
Administrator <2084267015@qq.com>
2019-07-23 22:09:39 +0800

    stream use

---
+++ homepage-service/homepage-course/src/main/java/com/homepage/controller/HomepageCourseController.java
@@ -0,0 +1,35 @@
+package com.homepage.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.homepage.CourseInfo;
+import com.homepage.CourseInfosRequest;
+import com.homepage.service.ICourseService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+public class HomepageCourseController {
+
+    @Autowired
+    private ICourseService courseService;
+
+    @GetMapping("/get/course")
+    public CourseInfo getCourseInfo(Long id) {
+        log.info("course id:{}", id);
+        return courseService.getCourseInfo(id);
+    }
+
+    @PostMapping("/get/courses")
+    public List<CourseInfo> getCourseInfos(@RequestBody CourseInfosRequest request) {
+        log.info("course ids:{}", JSON.toJSONString(request));
+        return courseService.getCourseInfos(request);
+    }
+
+}
