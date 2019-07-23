3c08a169ee62ccfd7c4c422c5dd5e9bbb695829b
Administrator <2084267015@qq.com>
2019-07-23 21:50:03 +0800

    service

---
+++ homepage-service/homepage-course/src/main/java/com/homepage/dao/HomepageCourseRepo.java
@@ -0,0 +1,8 @@
+package com.homepage.dao;
+
+import com.homepage.entity.HomepageCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface HomepageCourseRepo extends JpaRepository<HomepageCourse, Long> {
+
+}
---
+++ homepage-service/homepage-course/src/main/java/com/homepage/service/ICourseService.java
@@ -0,0 +1,13 @@
+package com.homepage.service;
+
+import com.homepage.CourseInfo;
+import com.homepage.CourseInfosRequest;
+
+import java.util.List;
+
+public interface ICourseService {
+
+    CourseInfo getCourseInfo(Long id);
+
+    List<CourseInfo> getCourseInfos(CourseInfosRequest request);
+}
