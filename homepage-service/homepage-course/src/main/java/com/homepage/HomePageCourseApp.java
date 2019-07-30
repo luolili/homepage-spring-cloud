5ac1113ad232c67e4aec72bc8f409062d814ad21
Administrator <2084267015@qq.com>
2019-07-30 21:48:22 +0800

    cacheable

--- homepage-service/homepage-course/src/main/java/com/homepage/HomePageCourseApp.java
+++ homepage-service/homepage-course/src/main/java/com/homepage/HomePageCourseApp.java
@@ -2,6 +2,7 @@ package com.homepage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,6 +11,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableEurekaClient
 @SpringBootApplication
 @EnableAspectJAutoProxy(proxyTargetClass = true)//强制使用cglib
+@EnableCaching//开启aop的缓存，否则他不会有缓存的功能
 public class HomePageCourseApp {
     public static void main(String[] args) {
         SpringApplication.run(HomePageCourseApp.class, args);
