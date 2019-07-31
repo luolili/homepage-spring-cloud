18e7f3f010c7afce3dc668ad6ab42ac20b4a934d
Administrator <2084267015@qq.com>
2019-07-31 21:23:26 +0800

    aop practice

--- homepage-service/homepage-course/src/main/java/com/homepage/util/DiffUtils.java
+++ homepage-service/homepage-course/src/main/java/com/homepage/util/DiffUtils.java
@@ -1,16 +1,22 @@
 package com.homepage.util;
 
+import com.homepage.entity.ChangeItem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class DiffUtils {
     //log
     private static final Logger logger = LoggerFactory.getLogger(DiffUtils.class);
 
-    public Object getObjectById(Object target, Object id) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+    //静态方法
+    public static Object getObjectById(Object target, Object id) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         //通过obj获取到他的Class,通过Class获取到指定的方法
         Method findMethod = target.getClass().getDeclaredMethod("findById", Long.class);
         //通过Method.invoke获取方法所属的类对象
@@ -18,6 +24,54 @@ public class DiffUtils {
         return oldObj;
 
     }
+
+    public static List<ChangeItem> getInsertChangeItems(Object obj) {
+
+
+        return null;
+    }
+
+
+    /**
+     * 获取bean 的filed name and value
+     *
+     * @param bean       bean
+     * @param filterNull
+     * @return
+     */
+    public static Map<String, String> getBeanSimpleFieldValueMap(Object bean, boolean filterNull) {
+
+
+        Map<String, String> map = new HashMap<>();
+
+        if (bean == null) {
+            return map;
+        }
+
+        Class<?> clazz = bean.getClass();
+        try {
+            //不获取父类的i段
+            Field[] fields = clazz.getDeclaredFields();
+            for (int i = 0; i < fields.length; i++) {
+
+                Class<?> fieldType = fields[i].getType();
+                String name = fields[i].getName();
+                Method method = clazz.getMethod("get" + name.substring(0, 1).toUpperCase() + name.substring(1));
+
+                Object value = method.invoke(bean);
+                if (filterNull && value == null) {
+                    continue;
+                }
+
+
+            }
+        } catch (Exception e) {
+
+        }
+
+
+    }
+
 
 
 }
