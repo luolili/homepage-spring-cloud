3159565967c6fe2b80088d952b681984b92df305
Administrator <2084267015@qq.com>
2019-08-04 17:17:32 +0800

    netty config

---
+++ homepage-service/homepage-user/src/main/java/com/homepage/config/MyWebSocketChannelHandler.java
@@ -0,0 +1,20 @@
+package com.homepage.config;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.stream.ChunkedWriteHandler;
+
+/**
+ * 初始化连接时的组件们
+ */
+public class MyWebSocketChannelHandler extends ChannelInitializer<SocketChannel> {
+    @Override
+    protected void initChannel(SocketChannel ch) throws Exception {
+        ch.pipeline().addLast("http-codec", new HttpServerCodec());
+        ch.pipeline().addLast("aggregator", new HttpObjectAggregator(65536));
+        ch.pipeline().addLast("http-chunk", new ChunkedWriteHandler());
+        ch.pipeline().addLast("handler", new MyWebSocketHandler());
+    }
+}
