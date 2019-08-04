7e6f4fe4507c71df8af248dd4c8d4916d3ce7073
Administrator <2084267015@qq.com>
2019-08-04 14:23:09 +0800

    netty config

---
+++ homepage-service/homepage-user/src/main/java/com/homepage/config/NettyConfig.java
@@ -0,0 +1,12 @@
+package com.homepage.config;
+
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.GlobalEventExecutor;
+
+public class NettyConfig {
+
+    public static ChannelGroup group =
+            new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
+}
